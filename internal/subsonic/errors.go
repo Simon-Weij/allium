c6cb5a725e8dd5cf1c461b8d295cb3895a29b6ae
Chaitanya <pancaakeman@gmail.com>
2026-08-17 20:59:42 +0100

    feat: add more subsonic errors

--- internal/subsonic/errors.go
+++ internal/subsonic/errors.go
@@ -1,8 +1,10 @@
 package subsonic
 
 const (
-	ErrWrongCredentials                    = 10
+	ErrGeneric = 0
+	ErrWrongCredentials                    = 20
 	ErrNotSupported                        = 42
 	ErrConflictingAuthenticationMechanisms = 43
 	ErrParameterMissing                    = 10
+	ErrRequestedDataNotFound = 70
 )
