c127e4c55b5d4525fccc55afbf2f75934472e00c
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-08-31 20:27:00 +0200

    feat: get recommendations from reccobeats

--- internal/resolver/listenbrainz.go
+++ internal/resolver/listenbrainz.go
@@ -1,15 +1,41 @@
 package resolver
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+)
 
-type SpotifyIDResponse struct {
-	SpotifyTrackIDs []string `json:"spotify_track_ids"`
-}
+type (
+	SpotifyIDResponse struct {
+		SpotifyTrackIDs []string `json:"spotify_track_ids"`
+	}
+	ListenBrainzWrapper struct {
+		Content []ListenBrainzSongs `json:"content"`
+	}
+	ListenBrainzSongs struct {
+		Id                 string                `json:"id"`
+		TrackTitle         string                `json:"string"`
+		Artists            []ListenBrainzArtists `json:"artists"`
+		DurationMs         int                   `json:"durationMs"`
+		Isrc               string                `json:"isrc"`
+		Ean                string                `json:"ean"`
+		Upc                string                `json:"upc"`
+		Href               string                `json:"href"`
+		AvaliableCountries string                `json:"availableCountries"`
+		Popularity         int                   `json:"popularity"`
+	}
+	ListenBrainzArtists struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+		Href string `json:"href"`
+	}
+)
 
 // GetSpotifyIdByMetadata exists we can get the spotify id, so reccobeats can eventually recommend similar songs.
 func (r Resolver) GetSpotifyIdByMetadata(
-	artistName string,
-	albumName string,
+	artistName,
+	albumName,
 	title string,
 ) (string, error) {
 	var result SpotifyIDResponse
@@ -27,4 +53,25 @@ func (r Resolver) GetSpotifyIdByMetadata(
 	}
 
 	return result.SpotifyTrackIDs[0], nil
+}
+
+func (r Resolver) GetRecommendationsFromReccobeats(seeds []string, size int) error {
+	var result ListenBrainzWrapper
+
+	params := url.Values{}
+	for _, seed := range seeds {
+		params.Add("seeds", seed)
+	}
+
+	params.Add("size", strconv.Itoa(size))
+
+	_, err := r.client.R().
+		SetQueryParamsFromValues(params).
+		SetResult(&result).
+		Get("https://api.reccobeats.com/v1/track/recommendation")
+	if err != nil {
+		return fmt.Errorf("could not get recommendations: %w", err)
+	}
+
+	return nil
 }
